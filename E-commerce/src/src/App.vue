40a7a7d8890ac559e6e42484030cb350960671aa
Diego Alejandro Ocampo Madroñero <134111264+MAKEBUZ@users.noreply.github.com>
2024-11-07 15:21:36 -0500

    2DO

--- E-commerce/src/src/App.vue
+++ E-commerce/src/src/App.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
 import Header from './components/Header.vue'
 import Footer from './components/Footer.vue'
-import router from './router';
 
 </script>
 
 <template>
    <Header />
    <RouterView />
-   <Footer />
+    <Footer />
 </template>
 
 <style lang="css">
---
+++ E-commerce/src/src/views/AboutView.vue
@@ -0,0 +1,12 @@
+<script lang="ts">
+</script>
+<template>
+  <div class="about">
+      <h1>About Us</h1>
+      <p>Welcome to our E-commerce site! We offer a wide range of products for all your needs.</p>
+  </div>
+</template>
+
+<style>
+
+</style>
